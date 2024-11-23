b7be09c269f7eee1e1e01d48ed80b7ae185c0a99
HUNTER_KARAN <KUMAR663959@GMAIL.COM>
2024-11-24 04:47:10 +0530

    Update autoleave.py

--- Hunter/plugins/misc/autoleave.py
+++ Hunter/plugins/misc/autoleave.py
@@ -2,9 +2,9 @@ import asyncio
 from datetime import datetime
 
 import config
-from HydraMusic import app
-from HydraMusic.core.call import Hydra, autoend
-from HydraMusic.utils.database import get_client, is_active_chat, is_autoend
+from Hunter import app
+from Hunter.core.call import Hydra, autoend
+from Hunter.utils.database import get_client, is_active_chat, is_autoend
 
 
 async def auto_leave():
@@ -26,9 +26,9 @@ async def auto_leave():
                             chat_id = i.chat.id
                             if (
                                 chat_id != config.LOG_GROUP_ID
-                                and chat_id != -1001190342892
-                                and chat_id != -1001733534088
-                                and chat_id != -1001443281821
+                                and chat_id != -1001786564428
+                                and chat_id != 
+                                and chat_id !=
                             ):
                                 if left == 20:
                                     continue
